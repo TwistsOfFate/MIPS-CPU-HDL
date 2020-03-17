6116ce10f43a98817b4f7b92cb50b047c1941b1f
Fan <yu_fanqi@163.com>
2020-03-18 07:00:23 +0800

    Supported all 20 instructions and passed all tests.

--- Single-Cycle-MIPS/Single-Cycle-MIPS.srcs/sources_1/new/control.sv
+++ Single-Cycle-MIPS/Single-Cycle-MIPS.srcs/sources_1/new/control.sv
@@ -25,46 +25,66 @@ module control(
     input logic Zero,
     output logic MemtoReg, MemWrite,
     output logic [2:0] ALUControl,
-    output logic ALUSrc, RegDst, RegWrite,
-    output logic [1:0] PCSrc
+    output logic ALUSrc,
+    output logic [1:0] RegDst,
+    output logic RegWrite,
+    output logic [1:0] PCSrc, Shift,
+    output logic Link, Sign
     );
     
-    logic [1:0] ALUOp;
-    logic [4:0] Out;
+    logic [2:0] ALUOp;
+    logic [6:0] Out;
+    
+    assign {RegWrite, RegDst[1:0], ALUSrc, MemWrite, MemtoReg, Sign} = Out;
     
     always_comb
         case (Op)
-            6'b000000: {Out, ALUOp} <= 'b11000_10;   //R-type
-            6'b100011: {Out, ALUOp} <= 'b10101_00;   //rw
-            6'b101011: {Out, ALUOp} <= 'b0X11X_00;   //sw
-            6'b000100: {Out, ALUOp} <= 'b0X00X_01;   //beq
-            6'b001000: {Out, ALUOp} <= 'b10100_00;   //addi
-            6'b000010: {Out, ALUOp} <= 'b0XX0X_XX;   //jump
-            6'b000101: {Out, ALUOp} <= 'b0X00X_01;     //bne
+            6'b000000: {Out, ALUOp} <= 'b1_01_000X_011;   //R-type
+            6'b100011: {Out, ALUOp} <= 'b1_00_1011_010;   //lw
+            6'b101011: {Out, ALUOp} <= 'b0_XX_11X1_010;   //sw
+            6'b000100: {Out, ALUOp} <= 'b0_XX_00X1_110;   //beq
+            6'b001000: {Out, ALUOp} <= 'b1_00_1001_010;   //addi
+            6'b000010: {Out, ALUOp} <= 'b0_XX_X0XX_XXX;   //jump
+            6'b000101: {Out, ALUOp} <= 'b0_XX_00X1_110;     //bne
+            6'b000011: {Out, ALUOp} <= 'b1_10_X0XX_XXX;   //jal
+            6'b001010: {Out, ALUOp} <= 'b1_00_1001_111;     //slti
+            6'b001100: {Out, ALUOp} <= 'b1_00_1000_000;     //andi
+            6'b001011: {Out, ALUOp} <= 'b1_00_1000_001;     //ori
             default: ;
         endcase
     
     always_comb
         casex ({ALUOp, Funct})
-            8'b00_XXXXXX: ALUControl <= 3'b010;     //add
-            8'bX1_XXXXXX: ALUControl <= 3'b110;     //subtract
-            8'b1X_100000: ALUControl <= 3'b010;     //add
-            8'b1X_100010: ALUControl <= 3'b110;     //subtract
-            8'b1X_100100: ALUControl <= 3'b000;     //and
-            8'b1X_100101: ALUControl <= 3'b001;     //or
-            8'b1X_101010: ALUControl <= 3'b111;     //set less than
-            8'b1X_001000: ALUControl <= 3'b010;     //jr
-            default: ;
+            9'b011_100000: ALUControl <= 3'b010;     //add
+            9'b011_100010: ALUControl <= 3'b110;     //subtract
+            9'b011_100100: ALUControl <= 3'b000;     //and
+            9'b011_100101: ALUControl <= 3'b001;     //or
+            9'b011_101010: ALUControl <= 3'b111;     //set less than
+            9'b011_001000: ALUControl <= 3'b010;     //jr
+            9'b011_000000: ALUControl <= 3'b010;     //sll
+            9'b011_000010: ALUControl <= 3'b010;     //srl
+            9'b011_000011: ALUControl <= 3'b010;     //sra
+            default: ALUControl <= ALUOp[2:0];
         endcase
     
     always_comb
     begin
-        if (Op==6'b000010) PCSrc <= 2'b10;
+        if (Op==6'b000010||Op==6'b000011) PCSrc <= 2'b10;
         else if (Op==6'b000000&&Funct==6'b001000) PCSrc <= 2'b11;
         else if (Op==6'b000100&&Zero || Op==6'b000101&&~Zero) PCSrc <= 2'b01;
         else PCSrc <= 2'b00;
     end
     
-    assign {RegWrite, RegDst, ALUSrc, MemWrite, MemtoReg} = Out;
+    always_comb
+    begin
+        if ({Op, Funct}==12'b000000_000000) Shift <= 2'b01;              //sll
+        else if ({Op, Funct}==12'b000000_000010) Shift <= 2'b10;         //srl
+        else if ({Op, Funct}==12'b000000_000011) Shift <= 2'b11;         //sra
+        else Shift <= 2'b00;
+    end
+    
+    always_comb
+        if (Op==6'b000011) Link <= 1'b1;
+        else Link <= 1'b0;
     
 endmodule
--- Single-Cycle-MIPS/Single-Cycle-MIPS.srcs/sources_1/new/mips.sv
+++ Single-Cycle-MIPS/Single-Cycle-MIPS.srcs/sources_1/new/mips.sv
@@ -32,31 +32,35 @@ module mips #(parameter ADDRLEN=32, DATALEN=32) (
     );
     
     logic [ADDRLEN-1:0] next_PC, PC;
-    logic [DATALEN-1:0] Instr, SrcA, SrcB, ALUResult, ReadData, WriteData, SignImm, Result, PCPlus4, PCBranch, mux2_pcsrc_y;
+    logic [DATALEN-1:0] Instr, SrcA, SrcB, ALUResult, ReadData, WriteData, ExtImm, Result, PCPlus4, PCBranch, mux2_pcsrc_y, ShiftResult, RegData;
     logic [2:0] ALUControl;
-    logic RegWrite, MemWrite, ALUSrc, MemtoReg, RegDst, Zero, Branch, Jump, BranchN;
+    logic RegWrite, MemWrite, ALUSrc, MemtoReg, Zero, Link, Sign;
     logic [4:0] WriteReg;
-    logic [1:0] PCSrc;
+    logic [1:0] PCSrc, RegDst, Shift;
     
     assign Instr = instr;
     assign ReadData = readdata;
     
     pcounter pcounter(.clk(clk), .reset(reset), .next_pc(next_PC), .cur_pc(PC));
-    regfile regfile(.clk(clk), .reset(reset), .we3(RegWrite), .a1(Instr[25:21]), .a2(Instr[20:16]), .a3(WriteReg), .rd1(SrcA), .rd2(WriteData), .wd3(Result));
-    signext signext(.in(Instr[15:0]), .out(SignImm));
+    regfile regfile(.clk(clk), .reset(reset), .we3(RegWrite), .a1(Instr[25:21]), .a2(Instr[20:16]), .a3(WriteReg), .rd1(SrcA), .rd2(WriteData), .wd3(RegData));
+    immextend immextend(.short(Instr[15:0]), .sign(Sign), .long(ExtImm));
     alu alu(.a(SrcA), .b(SrcB), .f(ALUControl), .zf(Zero), .y(ALUResult));
     adder adder_pcplus4(.a(PC), .b(4), .y(PCPlus4));
-    mux2 mux2_alusrc(.d0(WriteData), .d1(SignImm), .s(ALUSrc), .y(SrcB));
-    mux2 mux2_memtoreg(.d0(ALUResult), .d1(ReadData), .s(MemtoReg), .y(Result));
-    mux2 #(5) mux2_regdst(.d0(Instr[20:16]), .d1(Instr[15:11]), .s(RegDst), .y(WriteReg));
-    adder adder_pcbranch(.a({SignImm[DATALEN-3:0], 2'b00}), .b(PCPlus4), .y(PCBranch));
+    mux2 mux2_alusrc(.d0(WriteData), .d1(ExtImm), .s(ALUSrc), .y(SrcB));
+    mux2 mux2_memtoreg(.d0(ShiftResult), .d1(ReadData), .s(MemtoReg), .y(Result));
+    mux4 #(5) mux2_regdst(.d0(Instr[20:16]), .d1(Instr[15:11]), .d2(5'd31), .d3(5'd0), .s(RegDst), .y(WriteReg));
+    adder adder_pcbranch(.a({ExtImm[DATALEN-3:0], 2'b00}), .b(PCPlus4), .y(PCBranch));
     mux4 mux4_pcsrc(.d0(PCPlus4), .d1(PCBranch), .d2({PCPlus4[31:28], Instr[25:0], 2'b00}), .d3(Result), .s(PCSrc), .y(next_PC));
-    control control(.Op(Instr[31:26]), .Funct(Instr[5:0]), .Zero(Zero), .MemtoReg(MemtoReg), .MemWrite(MemWrite),
-                     .ALUControl(ALUControl), .ALUSrc(ALUSrc), .RegDst(RegDst), .RegWrite(RegWrite), .PCSrc(PCSrc));
+    shifter shifter(.a(ALUResult), .b(Instr[10:6]), .f(Shift), .y(ShiftResult));
+    mux2 mux2_link(.d0(Result), .d1(PCPlus4), .s(Link), .y(RegData));
+    
+    control control(.Op(Instr[31:26]), .Funct(Instr[5:0]), .Zero(Zero), .MemtoReg(MemtoReg), .MemWrite(MemWrite), .ALUControl(ALUControl),
+                     .ALUSrc(ALUSrc), .RegDst(RegDst), .RegWrite(RegWrite), .PCSrc(PCSrc), .Shift(Shift), .Link(Link), .Sign(Sign));
+    
         
     assign pc = PC;
     assign memwrite = MemWrite;
-    assign aluout = ALUResult;
+    assign aluout = ShiftResult;
     assign writedata = WriteData;
     
     
